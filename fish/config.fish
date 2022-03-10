0bebffd2af18826c514cc76a7fddb5a4bed4dc20
Kaan Genc <kaan@bgenc.net>
2022-03-09 20:58:58 -0500

    use flatpak desktop files

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# set up direnv
 direnv hook fish | source
+
+# set current desktop, needed for desktop sharing with Wayland
 set -gx XDG_CURRENT_DESKTOP sway
+
+# load desktop files from flatpak
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
