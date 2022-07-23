6919b66f03c785f5d6f8455f1cae5bf1f8a64a76
Kaan Barmore-Genc <kaan@bgenc.net>
2022-07-23 13:34:25 -0400

    add work specific changes

--- fish/config.fish
+++ fish/config.fish
@@ -4,23 +4,6 @@ end
 # set up direnv
 direnv hook fish | source
 
-# set current desktop, needed for desktop sharing with Wayland
-set -gx XDG_CURRENT_DESKTOP sway
-
-# make Qt apps use the theme set by qt5ct
-set -gx QT_QPA_PLATFORMTHEME qt5ct
-
-# load desktop files from flatpak
-set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
-set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-
-# Unlock login keyring
-if test -n "$DESKTOP_SESSION"
-    set -x (gnome-keyring-daemon --start | string split "=")
-end
-
-for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
-    if test -d $flatpakdir
-        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
-    end
-end
+# Set Mac keyboard shortcuts
+bind \u0192 forward-word
+bind \u222B backward-word
